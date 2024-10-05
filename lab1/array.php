83451908955d310a55f68f04629ae8c8d0dcb2b1
Andrew ImDoctor <andrew@imdoc.ru>
2024-10-05 15:03:34 +0300

    Update array.php
    Task made

--- lab1/array.php
+++ lab1/array.php
@@ -1,15 +1,26 @@
 <?php
-/*
-ЗАДАНИЕ 1
-- C помощью короткого синтаксиса массива cоздайте массив $bmw с ключами:
-	'model'
-	'speed, km/h'
-	'doors'
-	'year'
-- Заполните ячейки значениями: 'X5', 120, 5, '2006'	
-- Создайте массивы $toyota и $opel аналогичные массиву $bmw.
-- Заполните массив $toyota значениями: 'Carina', 130, 4, '2007'
-- Заполните массив $opel значениями: 'Corsa', 140, 5, '2007'		
+//ЗАДАНИЕ 1
+//массивы bmw, toyota, opel
+$bmw = [
+    'model' => "x5",
+	'speed, km/h' => 120, 
+	'doors' => 5,
+	'year' => '2006'
+];
+
+$toyota = [
+	'model' => 'Carina',
+	'speed, km/h' => 130,
+	'doors' => 4,
+	'year' => '2007'
+];
+
+$opel = [
+	'model' => 'Corsa',
+	'speed, km/h' => 140,
+	'doors' => 5,
+	'year' => '2007'
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -26,6 +37,9 @@
 	ЗАДАНИЕ 2
 	- С помощью подстановки в строку выведите значения всех трёх массивов в виде: name - model - speed - doors -year,  например: bmw - x5 - 120 - 5 - 2006
 	*/
+    echo "BMW - {$bmw['model']} - {$bmw['speed, km/h']} - {$bmw['doors']} - {$bmw['year']}<br>";
+	echo "Toyota - {$toyota['model']} - {$toyota['speed, km/h']} - {$toyota['doors']} - {$toyota['year']}<br>";
+	echo "Opel - {$opel['model']} - {$opel['speed, km/h']} - {$opel['doors']} - {$opel['year']}<br>";
 	?>
 
 </body>
