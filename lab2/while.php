67ed806849e884a69b677e50f887901223c3e794
Andrew 'ImDoctor <andrew@imdoc.ru>
2024-10-12 00:04:12 +0300

    Create while.php
    Copy the template

---
+++ lab2/while.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Цикл while</title>
+</head>
+<body>
+	<h1>Цикл while</h1>
+	<?php
+	/*
+	ЗАДАНИЕ
+	- Создайте переменную $var и присвойте ей строковое значение HELLO
+	- Используя цикл while выведите значение переменной $var в столбик так, 
+	  чтобы на выходе в браузере получилось:
+	H
+	E
+	L
+	L
+	O
+	*/
+	?> 
+</body>
+</html>
