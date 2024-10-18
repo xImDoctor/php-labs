e1297fa1b528b119d4f7ab829a3fbc8ba98cdafe
Andrew 'ImDoctor <andrew@imdoc.ru>
2024-10-18 23:39:15 +0300

    Upload test prjct folder

---
+++ lab3/site/table.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Таблица умножения</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <!-- Верхняя часть страницы -->
+    <img src="logo.png" width="130" height="80" alt="Наш логотип" class="logo">
+    <span class="slogan">приходите к нам учиться</span>
+    <!-- Верхняя часть страницы -->
+  </header>
+  <section>
+    <!-- Заголовок -->
+    <h1>Таблица умножения</h1>
+    <!-- Заголовок -->
+    <!-- Область основного контента -->
+    <form action=''>
+      <label>Количество колонок: </label>
+      <br>
+      <input name='cols' type='text' value=''>
+      <br>
+      <label>Количество строк: </label>
+      <br>
+      <input name='rows' type='text' value=''>
+      <br>
+      <label>Цвет: </label>
+      <br>
+      <input name='color' type='color' value='#ff0000' list="listColors">
+	<datalist id="listColors">
+		<option>#ff0000</option>/>
+		<option>#00ff00</option>
+		<option>#0000ff</option>
+	</datalist>
+      <br>
+      <br>
+      <input type='submit' value='Создать'>
+    </form>
+    <br>
+    <!-- Таблица -->
+    <table border='1' width='200'>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>4</td>
+        <td>6</td>
+        <td>8</td>
+        <td>10</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>6</td>
+        <td>9</td>
+        <td>12</td>
+        <td>15</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>8</td>
+        <td>12</td>
+        <td>16</td>
+        <td>10</td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>10</td>
+        <td>15</td>
+        <td>20</td>
+        <td>25</td>
+      </tr>
+    </table>
+    <!-- Таблица -->
+    <!-- Область основного контента -->
+  </section>
+  <nav>
+    <h2>Навигация по сайту</h2>
+    <!-- Меню -->
+    <ul>
+      <li><a href='index.php'>Домой</a></li>
+      <li><a href='about.php'>О нас</a></li>
+      <li><a href='contact.php'>Контакты</a></li>
+      <li><a href='table.php'>Таблица умножения</a></li>
+      <li><a href='calc.php'>Калькулятор</a></li>
+    </ul>
+    <!-- Меню -->
+  </nav>
+  <footer>
+    <!-- Нижняя часть страницы -->
+    &copy; Супер Мега Веб-мастер, 2000 &ndash; 20xx
+    <!-- Нижняя часть страницы -->
+  </footer>
+</body>
+</html>
