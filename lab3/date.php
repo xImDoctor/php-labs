da36fb26f5cb0ada1042ef137bbcc70dfaca74dc
Andrew 'ImDoctor <andrew@imdoc.ru>
2024-10-18 23:12:33 +0300

    Update date.php

--- lab3/date.php
+++ lab3/date.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /*
    ЗАДАНИЕ 1
    - Присвойте переменной $now значение метки времени актуальной даты(сегодня)
@@ -6,6 +7,11 @@
    - Создайте переменную $hour
    - С помощью функции getdate() присвойте переменной $hour текущий час
    */
+
+$now = time();
+$birthday = mktime(0, 0, 0, 10, 9, 2004);
+$hour = getdate()['hours'];
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -39,7 +45,34 @@
        - На отдельной строке выведите фразу "До моего дня рождения осталось "
        - Выведите количество дней, часов, минут и секунд оставшееся до Вашего дня рождения
        */
+
+    if ($hour >= 0 && $hour < 6)
+        $welcome = 'Доброй ночи';
+    elseif ($hour >= 6 && $hour < 12)
+        $welcome = 'Доброе утро';
+    elseif ($hour >= 12 && $hour < 18)
+        $welcome = 'Добрый день';
+    elseif ($hour >= 18 && $hour < 23)
+        $welcome = 'Добрый вечер';
+    else
+        $welcome = 'Доброй ночи';
+
+
+    setlocale(LC_TIME, 'ru_RU.UTF-8');
+    $formattedDate = datefmt_format('Сегодня %e %B %Y года, %A %H:%M:%S', $now);
+
+    $timeBeforeBirthday = $birthday - $now;
+    $days = floor($timeToBirthday / (60 * 60 * 24));
+    $hours = floor(($timeToBirthday % (60 * 60 * 24)) / (60 * 60));
+    $minutes = floor(($timeToBirthday % (60 * 60)) / 60);
+    $seconds = $timeToBirthday % 60;
+
     ?>
+
+    <p><?= $welcome ?>
+    <p><?= $formattedDate ?>
+    <p>До моего дня рождения: <?= "$days дней, $hours часов, $minutes минут, $seconds секунд" ?>
+
 </body>
 
 </html>
