9475b8f234ffd40eb207fe765e15e4ebae92f008
Andrew 'ImDoctor <andrew@imdoc.ru>
2024-10-19 11:38:09 +0300

    Update index.php
    Link to include file updated

--- lab4/site/index.php
+++ lab4/site/index.php
@@ -60,7 +60,7 @@ switch ($id) {
         include 'tables.php';
         break;
       case 'calc':
-        include 'calc.php';
+        include 'calculator.php';
         break;
       default:
         include 'inc/index.inc.php';
