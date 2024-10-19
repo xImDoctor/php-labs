6444fe1fe5c37fbd99683aee55bd79a07326ce83
Andrew 'ImDoctor <andrew@imdoc.ru>
2024-10-19 11:59:24 +0300

    Fix color variable

--- lab4/site/inc/data.inc.php
+++ lab4/site/inc/data.inc.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 $cols = $_GET['cols'] ?? 5;
 $rows = $_GET['rows'] ?? 5;
-$color = $_GET['color'] ?? 'yellow';
+$color = $_GET['color'] ?? '#ffff00';
 
 
 $menu = [
--- lab4/site/tables.php
+++ lab4/site/tables.php
@@ -3,10 +3,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $cols = abs((int) $_POST['cols']);
   $rows = abs((int) $_POST['rows']);
   $color = trim(strip_tags($_POST['color']));
-}
-$cols = ($cols) ? $cols : 10;
-$rows = ($rows) ? $rows : 10;
-$color = ($color) ? $color : '#ffff00';
+} 
+
 ?>
 
 <!-- Область основного контента -->
@@ -22,11 +20,7 @@ $color = ($color) ? $color : '#ffff00';
   <label>Цвет: </label>
   <br>
   <input name='color' type='color' value='<?= $color ?>' list="listColors">
-  <datalist id="listColors">
-    <option>#ff0000</option>/>
-    <option>#00ff00</option>
-    <option>#0000ff</option>
-  </datalist>
+
   <br>
   <br>
   <input type='submit' value='Создать'>
