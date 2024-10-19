14d0f2b2163ab17f058d16fa8f5b51f233947f5d
Andrew 'ImDoctor <andrew@imdoc.ru>
2024-10-19 10:11:33 +0300

    Update data.inc.php
    Update href values

--- lab4/site/inc/data.inc.php
+++ lab4/site/inc/data.inc.php
@@ -8,8 +8,8 @@ $color = $_GET['color'] ?? 'yellow';
 
 $menu = [
     ['link' => 'Домой', 'href' => 'index.php'],
-    ['link' => 'О нас', 'href' => 'about.php'],
-    ['link' => 'Контакты', 'href' => 'contact.php'],
-    ['link' => 'Таблица умножения', 'href' => 'table.php'],
-    ['link' => 'Калькулятор', 'href' => 'calc.php'],
+    ['link' => 'О нас', 'href' => 'index.php?id=about'],
+    ['link' => 'Контакты', 'href' => 'index.php?id=contact'],
+    ['link' => 'Таблица умножения', 'href' => 'index.php?id=table'],
+    ['link' => 'Калькулятор', 'href' => 'index.php?id=calc'],
 ];
