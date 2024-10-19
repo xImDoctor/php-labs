7ad1427b3501233c6b026493041220f96a2901ff
Andrew 'ImDoctor <andrew@imdoc.ru>
2024-10-19 08:49:41 +0300

    Update index.php
    Include menu code to index.php

--- lab3/site/index.php
+++ lab3/site/index.php
@@ -85,8 +85,17 @@
     <h2>Навигация по сайту</h2>
     <!-- Меню -->
     <?php
+    include 'inc/lib.inc.php';
 
+    $menu = [
+      ['link' => 'Домой', 'href' => 'index.php'],
+      ['link' => 'О нас', 'href' => 'about.php'],
+      ['link' => 'Контакты', 'href' => 'contact.php'],
+      ['link' => 'Таблица умножения', 'href' => 'table.php'],
+      ['link' => 'Калькулятор', 'href' => 'calc.php'],
+    ];
 
+    getMenu($menu);
     ?>
     <!-- Меню -->
     <!-- Навигация -->
