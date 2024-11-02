d4fd5b04bc194d7ae348139f7c4c978157aff194
Andrew 'ImDoctor <andrew@imdoc.ru>
2024-11-02 11:12:28 +0300

    Update contact.php
    Removed unused parts

--- lab5/contact.php
+++ lab5/contact.php
@@ -9,13 +9,6 @@
 
 <body>
 
-  <header>
-    <!-- Верхняя часть страницы -->
-    <img src="logo.png" width="130" height="80" alt="Наш логотип" class="logo">
-    <span class="slogan">приходите к нам учиться</span>
-    <!-- Верхняя часть страницы -->
-  </header>
-
   <section>
     <!-- Заголовок -->
     <h1>Обратная связь</h1>
@@ -38,23 +31,7 @@
     </form>
     <!-- Область основного контента -->
   </section>
-  <nav>
-    <h2>Навигация по сайту</h2>
-    <!-- Меню -->
-    <ul>
-      <li><a href='index.php'>Домой</a>
-      </li>
-      <li><a href='about.php'>О нас</a>
-      </li>
-      <li><a href='contact.php'>Контакты</a>
-      </li>
-      <li><a href='table.php'>Таблица умножения</a>
-      </li>
-      <li><a href='calc.php'>Калькулятор</a>
-      </li>
-    </ul>
-    <!-- Меню -->
-  </nav>
+
   <footer>
     <!-- Нижняя часть страницы -->
     &copy; Супер Мега Веб-мастер, 2000 &ndash; 20xx
