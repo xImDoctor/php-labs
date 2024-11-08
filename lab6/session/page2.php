3d594e7eaeee2c0ba88b77c81047fdd77eff0b9d
Andrew 'ImDoctor <andrew@imdoc.ru>
2024-11-08 22:46:03 +0300

    Extract session.zip

---
+++ lab6/session/page2.php
@@ -0,0 +1,28 @@
+<?php
+// Открываем сессию
+session_start();
+
+// Подключаем код для сохранения информации о странице в сессии
+include('savepage.inc.php');
+?>
+<!doctype html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Страница 2</title>
+</head>
+<body>
+
+<h1>Страница 2</h1>
+
+<?php
+// Подключаем меню
+include('menu.inc.php');
+
+// Подключаем код для вывода информации обо всех посещенных страницах
+include('visited.inc.php');
+?>
+
+</body>
+</html>
