b9e47ddeabe8ca478e8f7642d13ce48a93347672
Andrew 'ImDoctor <andrew@imdoc.ru>
2024-11-08 22:50:44 +0300

    Update savepage.inc.php

--- lab6/session/savepage.inc.php
+++ lab6/session/savepage.inc.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 // Код для всех страниц - сохранение информации о посещенных страницах
 
 
@@ -9,4 +10,15 @@
 	- строку с уникальным разделителем и последовательно её дополняйте
 
 */
-?>
+
+ini_set("session.use_only_cookies", "0");
+ini_set("session.use_trans_sid", "1");
+
+if (!isset($_SESSION['visitedPages']))
+    $_SESSION['visitedPages'] = [];
+
+
+$currentPage = basename($_SERVER['PHP_SELF']);
+
+if (!in_array($currentPage, $_SESSION['visitedPages']))
+    $_SESSION['visitedPages'][] = $currentPage;
