c993d25fadba5eb8820a9c7e5e44348ebda2d03f
Andrew 'ImDoctor <andrew@imdoc.ru>
2024-11-08 22:53:25 +0300

    Update visited.inc.php

--- lab6/session/visited.inc.php
+++ lab6/session/visited.inc.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 // Код для всех страниц - вывод информации о посещенных страницах
 
 /*
@@ -9,4 +10,16 @@
 - Выводите в цикле список всех посещённых пользователем страниц
 
 */
-?>
+
+echo "<h2>Список посещённых страниц</h2>";
+
+
+if (isset($_SESSION['visitedPages']) && count($_SESSION['visitedPages']) > 0) {
+    
+	echo "<ul>";
+    foreach ($_SESSION['visitedPages'] as $page)
+        echo "<li>$page</li>";
+    echo "</ul>";
+	
+} else
+    echo "<p>Вы пока не посетили ни одну страницу :(</p>";
